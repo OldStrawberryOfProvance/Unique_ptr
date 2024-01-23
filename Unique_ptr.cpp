d37972a1bc194ed0f35dfab80a2f6f1f080b5f63
OldStrawberryOfProvance <152076246+OldStrawberryOfProvance@users.noreply.github.com>
2024-01-23 17:16:10 +0300

    Add files via upload

---
+++ Unique_ptr.cpp
@@ -0,0 +1,11 @@
+﻿// Unique_ptr.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+/
